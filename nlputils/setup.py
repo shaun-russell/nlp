b6d7ed9b2584583bc953921712659129bcb0d48e
Shaun Russell <shaun@ensemblemusic.co.nz>
2018-10-23 12:43:43 +1300

    setup didn't work but now it does

--- nlputils/setup.py
+++ nlputils/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name='nlputils',
   version='1.0',
-  py_modules=['nlputils.py','dedupe.py', 'untrash.py'],
+  py_modules=['nlputils','dedupe', 'untrash'],
   install_requires=['Click'],
   entry_points='''
     [console_scripts]
